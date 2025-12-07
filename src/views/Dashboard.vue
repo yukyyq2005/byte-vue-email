f49e26c4ebae23513d1067bf70194cc4e7a28cc7
yq <yukyyq2005@163.com>
2025-12-07 01:22:39 -0500

    m

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -25,7 +25,7 @@
             
 
             <div class="flex flex-row gap-x-1.5">
-                <Lang @switchLanguage="updateUILanguage" ref="langVue" class="mt-0.5"/>
+                <Lang @switchLanguage="updateUILanguage" ref="langVue" class="mt-0.5 ml-5"/>
                 <!-- 升级 -->
                 <button @click="upgradeButtonClick2" v-show="formStore.userPro==0" :title="upgradeDesc" class="relative btn ml-3">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-4 text-gray-500 mr-1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"></path></svg>
